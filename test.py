afc0ad63f4541de3be364fb3797bd545262f6e81
Tom Aldcroft <taldcroft@gmail.com>
2012-03-21 23:54:32 -0400

    Split tlmevent into module and test code

---
+++ test.py
@@ -0,0 +1,35 @@
+from itertools import izip, cycle
+
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from tlmevent import TlmEvent, Manvr
+
+Base = declarative_base()
+engine = create_engine('sqlite:///test.db3', echo=False)
+session = sessionmaker(bind=engine)()
+Base.metadata.create_all(engine)
+
+size = 1000
+msid_vals = ['pcadmd', 'aofatt', 'aoacaseq', 'cobsrqid']
+msids = (msid_vals[i] for i in np.random.randint(4, size=size))
+vals = cycle(['nman', 1, 2.3])
+priorvals = cycle([3.2, 'npnt', 1])
+
+# Make fake telemetry events
+for msid, time, priorval, val in izip(msids, xrange(size),
+                                      priorvals, vals):
+    tlmevent = TlmEvent(msid, float(time), priorval, val)
+    session.add(tlmevent)
+session.commit()
+
+# Make fake maneuvers
+for i in range(size // 10):
+    manvr = Manvr(session, i * 10.0, i * 10.0 + 10)
+    session.add(manvr)
+session.commit()
+
+events = session.query(TlmEvent).all()
+manvrs = session.query(Manvr).all()
--- tlmevent.py
+++ tlmevent.py
@@ -1,45 +1,43 @@
-from itertools import izip, cycle
-
-import numpy as np
 from sqlalchemy import Table
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy import PickleType
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
-
-manvr_event = Table('manvr_event', Base.metadata,
-                     Column('manvr_id', Integer,
-                            ForeignKey('manvr.id'), index=True,),
-                     Column('event_id', Integer, ForeignKey('event.id'))
-                     )
 
 
 class Manvr(Base):
     __tablename__ = 'manvr'
+    assoc_table = Table(__tablename__ + '_tlmevent',
+                        Base.metadata,
+                        Column('manvr_id', Integer,
+                               ForeignKey('manvr.id'), index=True),
+                        Column('tlmevent_id', Integer,
+                               ForeignKey('tlmevent.id'))
+                        )
+    tlmevents = relationship('TlmEvent', secondary=assoc_table,
+                             backref=__tablename__)
 
     id = Column(Integer, primary_key=True)
     tstart = Column(Float)
     tstop = Column(Float)
-    events = relationship('Event', secondary=manvr_event, backref='manvr')
 
-    def __init__(self, tstart, tstop):
+    def __init__(self, session, tstart, tstop):
         self.tstart = tstart
         self.tstop = tstop
-        events = session.query(Event).filter(Event.time >= tstart) \
-                                     .filter(Event.time <= tstop)
-        for event in events:
-            self.events.append(event)
+        tlmevents = session.query(TlmEvent).filter(TlmEvent.time >= tstart) \
+                                     .filter(TlmEvent.time <= tstop)
+        for tlmevent in tlmevents:
+            self.tlmevents.append(tlmevent)
 
     def __repr__(self):
-        return "<Manvr('{}','{}', '{}')>".format(
-            self.tstart, self.tstop, self.events)
+        return "<Manvr('{}','{}')>".format(
+            self.tstart, self.tstop)
 
 
-class Event(Base):
-    __tablename__ = 'event'
+class TlmEvent(Base):
+    __tablename__ = 'tlmevent'
 
     id = Column(Integer, primary_key=True)
     msid = Column(String)
@@ -54,32 +52,6 @@ class Event(Base):
         self.val = val
 
     def __repr__(self):
-        return "<Event({}, {}, {}, {})>".format(
+        return "<TlmEvent({}, {}, {}, {})>".format(
             repr(self.msid), repr(self.time),
             repr(self.priorval), repr(self.val))
-
-
-engine = create_engine('sqlite:///test.db3', echo=False)
-session = sessionmaker(bind=engine)()
-Base.metadata.create_all(engine)
-
-size = 100
-msid_vals = ['pcadmd', 'aofatt', 'aoacaseq', 'cobsrqid']
-msids = (msid_vals[i] for i in np.random.randint(4, size=size))
-for msid, time, priorval, val in izip(msids, xrange(size),
-                       cycle(['nman', 1, 2.3]),
-                       cycle([3.2, 'npnt', 1])):
-    event = Event(msid, float(time), priorval, val)
-    session.add(event)
-    if time % 10 == 0:
-        session.commit()
-session.commit()
-
-for i in range(size // 10):
-    manvr = Manvr(i * 10.0, i * 10.0 + 10)
-    session.add(manvr)
-
-session.commit()
-
-
-# timeit manvrs = session.query(Manvr).options(subqueryload(Manvr.events)).all()
