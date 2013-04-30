ff176a26710799d19f0544e765b8de88bf9d7db7
Tom Aldcroft <taldcroft@gmail.com>
2013-04-29 22:35:39 -0400

    Add functions to put and get files to/from lucky

--- kadi/occweb.py
+++ kadi/occweb.py
@@ -20,7 +20,6 @@ CACHE_DIR = 'cache'
 CACHE_TIME = 86000
 TIMEOUT = 60
 
-
 ROOTURL = 'http://occweb.cfa.harvard.edu'
 URLS = {'fdb_major_events':  '/occweb/web/fdb_web/Major_Events.html',
         'fot_major_events':  '/occweb/web/fot_web/eng/reports/Chandra_major_events.htm',
@@ -29,12 +28,12 @@ URLS = {'fdb_major_events':  '/occweb/web/fdb_web/Major_Events.html',
 
 
 def get_auth():
-        authfile = '/proj/sot/ska/data/aspect_authorization/occweb-{}'.format(os.environ['USER'])
-        config = configobj.ConfigObj(authfile)
-        username = config.get('username')
-        password = config.get('password')
+    authfile = '/proj/sot/ska/data/aspect_authorization/occweb-{}'.format(os.environ['USER'])
+    config = configobj.ConfigObj(authfile)
+    username = config.get('username')
+    password = config.get('password')
 
-        return (username, password)
+    return (username, password)
 
 
 def get_url(page, timeout=TIMEOUT):
@@ -88,3 +87,53 @@ def get_ifot(event_type, start=None, stop=None, props=[], columns=[], timeout=TI
                   for key, type_ in types.items()}
     dat = asciitable.read(lines, Reader=asciitable.Tab, guess=False, converters=converters)
     return dat
+
+
+def ftp_put_to_lucky(ftp_dirname, local_files, user='taldcroft', logger=None):
+    """Put the ``files`` onto lucky in /``user``/``ftp_dirname``. First put it at the top
+    level, then when complete move it into a subdir eng_archive.  This lets the OCC side
+    just watch for fully-uploaded files in that directory.
+    """
+    import Ska.File
+    import Ska.ftp
+    import uuid
+
+    ftp = Ska.ftp.FTP('lucky', logger=logger)
+    ftp.cd('/{}'.format(user))
+    files = ftp.nlst()
+
+    if ftp_dirname not in files:
+        ftp.mkd(ftp_dirname)
+
+    for local_file in local_files:
+        file_dir, file_base = os.path.split(os.path.abspath(local_file))
+        ftp_file = str(uuid.uuid4())  # random unique identifier
+        with Ska.File.chdir(file_dir):
+            ftp.put(file_base, ftp_file)
+            if logger:
+                logger.info('rename {} to {}/{}'.format(ftp_file, ftp_dirname, file_base))
+            ftp.rename(ftp_file, '{}/{}'.format(ftp_dirname, file_base))
+
+    ftp.close()
+
+
+def ftp_get_from_lucky(ftp_dirname, local_files, user='taldcroft', logger=None):
+    """
+    Get files from lucky.
+    """
+    import Ska.ftp
+    import Ska.File
+
+    ftp = Ska.ftp.FTP('lucky', logger=logger)
+    ftp.set_pasv(False)  # req'd on GRETA network
+    ftp.cd('/{}/{}'.format(user, ftp_dirname))
+    for local_file in local_files:
+        file_dir, file_base = os.path.split(os.path.abspath(local_file))
+        if file_base in ftp.ls():
+            with Ska.File.chdir(file_dir):
+                logger.info('Getting ftp file {} as {}'.format(file_base, local_file))
+                ftp.get(file_base)
+                logger.info('Deleting ftp {0}'.format(file_base))
+                ftp.delete(file_base)
+
+    ftp.close()
