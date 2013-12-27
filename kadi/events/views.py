ed8a8c0477d0d5e910139ac5d95dfc6680a1f9e6
Tom Aldcroft <taldcroft@gmail.com>
2013-12-27 18:13:15 -0500

    Refactor common parts of EventDetail and EventList into EventView

--- kadi/events/views.py
+++ kadi/events/views.py
@@ -25,7 +25,30 @@ class IndexView(TemplateView):
         return context
 
 
-class EventDetail(DetailView):
+class EventView(object):
+    """
+    Mixin for common stuff between EventDetail and EventList
+    """
+    def get_context_data(self, **kwargs):
+        context = super(EventView, self).get_context_data(**kwargs)
+        context['model_description'] = self.model.__doc__.strip().splitlines()[0]
+        context['model_name'] = MODEL_NAMES[self.model.__name__]
+        if self.filter_kwargs:
+            context['filter_url'] = '&'.join('{}={}'.format(key, val)
+                                             for key, val in self.filter_kwargs.items())
+
+        self.formats = {field.name: getattr(field, '_kadi_format', '{}')
+                        for field in self.model._meta.fields}
+
+        return context
+
+    def get_queryset(self):
+        self.filter_kwargs = {key: val for key, val in self.request.GET.items()
+                              if re.match(r'\w+__\w+$', key)}
+        return self.model.objects.filter(**self.filter_kwargs)
+
+
+class EventDetail(EventView, DetailView):
     template_name = 'events/event_detail.html'
 
     def get_context_data(self, **kwargs):
@@ -33,28 +56,17 @@ class EventDetail(DetailView):
         event = context['object']
         fields = self.model._meta.fields
         names = [field.name for field in fields]
-        formats = {field.name: getattr(field, '_kadi_format', '{}')
-                   for field in fields}
+        formats = self.formats
         context['names_vals'] = [(name, formats[name].format(getattr(event, name)))
                                  for name in names]
-        context['model_description'] = self.model.__doc__.strip().splitlines()[0]
-        context['model_name'] = MODEL_NAMES[self.model.__name__]
         context['next_event'] = event.get_next(**self.filter_kwargs)
         context['previous_event'] = event.get_previous(**self.filter_kwargs)
-        if self.filter_kwargs:
-            context['filter_url'] = '&'.join('{}={}'.format(key, val)
-                                             for key, val in self.filter_kwargs.items())
 
         return context
 
-    def get_queryset(self):
-        self.filter_kwargs = {key: val for key, val in self.request.GET.items()
-                              if re.match(r'\w+__\w+$', key)}
-        return self.model.objects.filter(**self.filter_kwargs)
 
-
-class EventList(ListView):
-    paginate_by = 20
+class EventList(EventView, ListView):
+    paginate_by = 30
     context_object_name = 'event_list'
     template_name = 'events/event_list.html'
 
@@ -62,26 +74,17 @@ class EventList(ListView):
         # Call the base implementation first to get a context
         context = super(EventList, self).get_context_data(**kwargs)
 
-        context['field_names'] = [field.name for field in self.model._meta.fields
+        fields = self.model._meta.fields
+        context['field_names'] = [field.name for field in fields
                                   if not hasattr(field, '_kadi_no_list_view')]
-        context['model_description'] = self.model.__doc__.strip().splitlines()[0]
-        context['model_name'] = MODEL_NAMES[self.model.__name__]
         event_list = context['event_list']
-        formats = {field.name: getattr(field, '_kadi_format', '{}')
-                   for field in self.model._meta.fields}
+        formats = self.formats
         context['event_rows'] = [[formats[name].format(getattr(event, name))
                                   for name in context['field_names']]
                                  for event in event_list]
-        if self.filter_kwargs:
-            context['filter_url'] = '&'.join('{}={}'.format(key, val)
-                                             for key, val in self.filter_kwargs.items())
+        context['filter'] = self.request.GET.get('filter', '')
 
         return context
-
-    def get_queryset(self):
-        self.filter_kwargs = {key: val for key, val in self.request.GET.items()
-                              if re.match(r'\w+__\w+$', key)}
-        return self.model.objects.filter(**self.filter_kwargs)
 
 
 # Define list view classes for each event model
