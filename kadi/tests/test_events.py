ee3e7489d297ed259aba34cae806af11e2c40b62
Tom Aldcroft <taldcroft@gmail.com>
2014-06-30 15:44:21 -0400

    Fix test_intervals_filter

--- kadi/tests/test_events.py
+++ kadi/tests/test_events.py
@@ -115,23 +115,28 @@ def test_intervals_filter():
     Test setting filter keywords in the EventQuery object itself.
     """
     ltt_bads = events.ltt_bads
-    start, stop = '2011:295', '2011:305'
+    start, stop = '2000:121', '2000:134'
 
-    assert (str(ltt_bads().filter('2011:298', '2011:305')).splitlines() ==
-            ['<LttBad: start=2011:299:04:58:39.000 msid=1CBAT flag=M>',
-             '<LttBad: start=2011:300:00:00:00.000 msid=AACCCDPT flag=A>',
-             '<LttBad: start=2011:301:00:00:00.000 msid=AACCCDPT flag=A>',
-             '<LttBad: start=2011:302:00:00:00.000 msid=AACCCDPT flag=A>'])
+    # 2000-04-30 00:00:00 | ELBI_LOW        | R
+    # 2000-04-30 00:00:00 | EPOWER1         | R
+    # 2000-05-01 00:00:00 | 3SDTSTSV        | Y
+    # 2000-05-13 00:00:00 | 3SDP15V         | 1
+
+    assert (str(ltt_bads().filter('2000:121', '2000:134')).splitlines() ==
+            ['<LttBad: start=2000:121:00:00:00.000 msid=ELBI_LOW flag=R>',
+             '<LttBad: start=2000:121:00:00:00.000 msid=EPOWER1 flag=R>',
+             '<LttBad: start=2000:122:00:00:00.000 msid=3SDTSTSV flag=Y>',
+             '<LttBad: start=2000:134:00:00:00.000 msid=3SDP15V flag=1>'])
 
     # No filter
     assert (ltt_bads.intervals(start, stop) ==
-            [('2011:299:04:58:39.000', '2011:303:00:00:00.000')])
+            [('2000:121:12:00:00.000', '2000:123:00:00:00.000'),
+             ('2000:134:00:00:00.000', '2000:134:12:00:00.000')])
 
-    assert (ltt_bads(flag='M').intervals(start, stop) ==
-            [('2011:299:04:58:39.000', '2011:300:04:58:39.000')])
+    assert (ltt_bads(flag='Y').intervals(start, stop) ==
+            [('2000:122:00:00:00.000', '2000:123:00:00:00.000')])
 
-    assert (ltt_bads(msid='AACCCDPT', flag='A').intervals(start, stop) ==
-            [('2011:300:00:00:00.000', '2011:303:00:00:00.000')])
+    assert (ltt_bads(msid='ELBI_LOW', flag='R').intervals(start, stop) ==
+            [('2000:121:12:00:00.000', '2000:122:00:00:00.000')])
 
-    assert (ltt_bads(msid='AACCCDPT', flag='A', start__gt='2011:301:12').intervals(start, stop) ==
-            [('2011:302:00:00:00.000', '2011:303:00:00:00.000')])
+    assert (ltt_bads(msid='ELBI_LOW', flag='1').intervals(start, stop) == [])
