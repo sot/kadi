d664c08b9d52971c4bb7f63db5b4b1695d84464d
Tom Aldcroft <taldcroft@gmail.com>
2014-08-20 10:41:21 -0400

    Update version to 0.11

--- kadi/version.py
+++ kadi/version.py
@@ -33,7 +33,7 @@ import os
 ### SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (0, 10, 1, False)
+VERSION = (0, 11, None, False)
 
 
 class SemanticVersion(object):
