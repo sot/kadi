ba66bf4ff44dcc58dd62799a00d9cae9a1c00be8
Tom Aldcroft <taldcroft@gmail.com>
2015-02-27 13:07:38 -0500

    Add find_attitude utility

--- kadi/settings.py
+++ kadi/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'kadi.events',
     'mica.web',
+    'find_attitude.web',
 )
 
 MIDDLEWARE_CLASSES = (
--- kadi/urls.py
+++ kadi/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 import kadi.views
 import mica.web.views
+import find_attitude.web.views
 
 admin.autodiscover()
 
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
                        url(r'^$', kadi.views.IndexView.as_view()),
                        url(r'^mica/$', mica.web.views.IndexView.as_view()),
                        url(r'^pcad_acq/$', mica.web.views.AcqView.as_view()),
+                       url(r'^find_attitude/$', find_attitude.web.views.index),
                        )
 
 # Another way to do this, corresponds to commented code in kadi/views.py
