dd83841475c962b3e2b447e2e57bb6a82b8f5f4e
Tom Aldcroft <taldcroft@gmail.com>
2017-07-11 16:37:43 -0400

    Make optional apps actually be optional

--- kadi/urls.py
+++ kadi/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import kadi.views
-import mica.web.views
-import find_attitude.web.views
-import Ska.engarchive.web
 
 admin.autodiscover()
 
@@ -12,13 +9,35 @@ urlpatterns = patterns('',
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^kadi/events/', include('kadi.events.urls')),
-                       url(r'^$', kadi.views.IndexView.as_view()),
-                       url(r'^mica/$', mica.web.views.IndexView.as_view()),
-                       url(r'^pcad_acq/$', mica.web.views.AcqView.as_view()),
-                       url(r'^star_hist/$', mica.web.views.StarHistView.as_view()),
-                       url(r'^find_attitude/$', find_attitude.web.views.index),
-                       url(r'^eng_archive/remote_func/$', Ska.engarchive.web.remote_func),
+                       url(r'^$', kadi.views.IndexView.as_view())
                        )
+
+# Other apps from mica, find_attitude and eng_archive are optional
+try:
+    import mica.web.views
+except ImportError:
+    pass
+else:
+    urlpatterns += patterns('',
+                            url(r'^mica/$', mica.web.views.IndexView.as_view()),
+                            url(r'^pcad_acq/$', mica.web.views.AcqView.as_view()),
+                            url(r'^star_hist/$', mica.web.views.StarHistView.as_view()))
+
+try:
+    import find_attitude.web.views
+except ImportError:
+    pass
+else:
+    urlpatterns += patterns('',
+                            url(r'^find_attitude/$', find_attitude.web.views.index))
+
+try:
+    import Ska.engarchive.web
+except ImportError:
+    pass
+else:
+    urlpatterns += patterns('',
+                            url(r'^eng_archive/remote_func/$', Ska.engarchive.web.remote_func))
 
 # Another way to do this, corresponds to commented code in kadi/views.py
 # urlpatterns = patterns('',
