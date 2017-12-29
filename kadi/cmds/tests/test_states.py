a837ed56d7a81f161259e65a6e7dbd462b058e6a
Tom Aldcroft <taldcroft@gmail.com>
2017-12-29 06:02:09 -0500

    Fix ACIS test

--- kadi/cmds/tests/test_states.py
+++ kadi/cmds/tests/test_states.py
@@ -11,13 +11,14 @@ def test_states_acis_simple():
                   'si_mode',  'ccd_count']
     cmds = commands.filter('2014:028', '2014:030:11:00:00')
     kstates = states.get_states_for_cmds(cmds, state_keys)
+    rkstates = states.reduce_states(kstates, state_keys)
 
     cstates = cmd_states.fetch_states('2014:030:02:50:00', '2014:030:11:00:00')
-    rstates = cmd_states.reduce_states(cstates, state_keys, allow_identical=False)
+    rcstates = states.reduce_states(cstates, state_keys)
 
-    lenr = len(rstates)
+    lenr = len(rcstates)
     for colname in ['datestart'] + state_keys:
-        assert np.all(kstates[-lenr:][colname] == rstates[colname])
+        assert np.all(rkstates[-lenr:][colname] == rcstates[colname])
 
 
 def test_states_manvr():
