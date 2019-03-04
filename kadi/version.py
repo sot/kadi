6b7ee26ceef4ef1fc83a949a21879dc6f1a4ed9d
Tom Aldcroft <taldcroft@gmail.com>
2019-03-04 12:06:03 -0500

    Update version to 3.16.4

--- kadi/version.py
+++ kadi/version.py
@@ -34,7 +34,7 @@ import os
 ### SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (3, 16, 3, False)
+VERSION = (3, 16, 4, False)
 
 
 class SemanticVersion(object):
