9be25a1fbe065fa8c5412bc4f241c683155eb682
Javier Gonzalez <javierggt@yahoo.com>
2019-09-30 14:52:21 -0400

    Updated version to 4.18.1
    Updated version to 4.18.1

--- kadi/version.py
+++ kadi/version.py
@@ -34,7 +34,7 @@ import os
 # SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (4, 18, 0, False)
+VERSION = (4, 18, 1, False)
 
 
 class SemanticVersion(object):
