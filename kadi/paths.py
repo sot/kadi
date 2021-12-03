2e15204d9f26bd5e500fe38a951e98feaa8ad558
Tom Aldcroft <taldcroft@gmail.com>
2021-12-03 11:43:05 -0500

    Migrate paths to all be Path objects

--- kadi/paths.py
+++ kadi/paths.py
@@ -4,23 +4,23 @@ from pathlib import Path
 
 
 def SKA_DATA():
-    return os.path.join(os.environ.get('SKA', '/proj/sot/ska'), 'data/kadi')
+    return Path(os.environ.get('SKA', '/proj/sot/ska'), 'data/kadi')
 
 
 def DATA_DIR():
-    return os.path.abspath(os.environ.get('KADI', SKA_DATA()))
+    return Path(os.environ.get('KADI', SKA_DATA())).absolute()
 
 
 def EVENTS_DB_PATH():
-    return os.path.join(DATA_DIR(), 'events3.db3')
+    return DATA_DIR() / 'events3.db3'
 
 
 def IDX_CMDS_PATH():
-    return os.path.join(DATA_DIR(), 'cmds.h5')
+    return DATA_DIR() / 'cmds.h5'
 
 
 def PARS_DICT_PATH():
-    return os.path.join(DATA_DIR(), 'cmds.pkl')
+    return DATA_DIR() / 'cmds.pkl'
 
 
 def CMDS_DIR(cmds_dir=None):
