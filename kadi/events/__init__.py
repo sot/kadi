9c36edc3ed8a22d39b6a09631007c34840d83292
Tom Aldcroft <taldcroft@gmail.com>
2022-04-25 15:45:27 -0400

    First round of cleanup

--- kadi/events/__init__.py
+++ kadi/events/__init__.py
@@ -43,21 +43,12 @@ More help available at:
 
 import os
 import django
-from django.conf import settings
-from django.utils.functional import empty
-
-# If we are running standalone then this ENV var is not set.  Need to explicitly
-# do the setup and then import query module attributes for standalone.
-# For WSGI server the env var is set in wsgi.py.
-# For the dev server it is set in manage.py.
 
 # In addition, set DJANGO_ALLOW_ASYNC_UNSAFE, to avoid exception seen running in
 # Jupyter notebook: SynchronousOnlyOperation: You cannot call this from an async
 # context. See: https://stackoverflow.com/questions/59119396
 
-if settings._wrapped is empty:
-    if 'DJANGO_SETTINGS_MODULE' not in os.environ:
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'kadi.settings'
-    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-    django.setup()
-    from .query import *  # noqa
+os.environ['DJANGO_SETTINGS_MODULE'] = 'kadi.settings'
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+from .query import *  # noqa
--- kadi/settings.py
+++ kadi/settings.py
@@ -11,29 +11,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 from __future__ import print_function
 
 # Build paths inside the project like this: join(BASE_DIR, ...)
-import sys
 import os
 from os.path import join, dirname, realpath
-os.environ.setdefault('SKA', '/proj/sot/ska')
-
-# Check if this instance is running in the production Apache kadi web server.
-# If so set XDG_CONFIG_HOME and friend to make sure that the astropy config is
-# stable for production and is not coming from the config in the user home dir.
-# This will put the config file in $SKA/data/kadi/config.
-#
-# This code relies on the fact that currently in this case (production) kadi is
-# installed into /proj/web-kadi and conf/httpd.conf contains: WSGIPythonPath
-# /proj/web-kadi/lib/python2.7/site-packages.
-#
-# See also
-# https://stackoverflow.com/questions/26979579/django-mod-wsgi-set-os-environment-variable-from-apaches-setenv
-# for useful commentary on problems just using SetEnv in the conf file.  Also
-# search email for XDG_CONFIG_HOME for more discussion and motivation.
-
-if any(pth.startswith('/proj/web-kadi') for pth in sys.path):
-    os.environ.setdefault('XDG_CONFIG_HOME',
-                          join(os.environ['SKA'], 'data', 'config'))
-    os.environ.setdefault('XDG_CACHE_HOME', os.environ['XDG_CONFIG_HOME'])
 
 BASE_DIR = dirname(dirname(realpath(__file__)))
 
@@ -84,44 +63,18 @@ except IOError:
             import warnings
             warnings.warn('Unable to change file mode permission!')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'kadi.cfa.harvard.edu',
-    'web-kadi.cfa.harvard.edu',
-    'kadi-test.cfa.harvard.edu',
-    'web-kadi-test.cfa.harvard.edu',
-]
-
 # Application definition
-
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
     'kadi.events',
     'mica.web',
     'find_attitude.web_find_attitude',  # app label (last module) must be unique
 )
-
-MIDDLEWARE = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'kadi.urls'
-
-WSGI_APPLICATION = 'kadi.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -132,74 +85,3 @@ DATABASES = {
         'NAME': EVENTS_DB_PATH(),
     }
 }
-
-# FORCE_SCRIPT_NAME = '/kadi'
-APPEND_SLASH = True
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-TIME_ZONE = 'America/New_York'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# OTHER Kadi customizations
-
-# Django admin static files are installed directly from the web-kadi repo via
-#   make install_admin_static
-
-STATICFILES_DIRS = (
-    #  Put strings here, like "/home/html/static" or "C:/www/django/static".
-    #  Always use forward slashes, even on Windows.
-    #  Don't forget to use absolute paths, not relative paths.
-    #
-    #  NOTE: It's possible to not define any STATICFILES_DIRS if the static
-    #        files are located in directories like kadi/events/static/.  Then
-    #        the staticfiles app will find them.  (E.g. /static/kadi.css would
-    #        be at kadi/events/static/kadi.css.)
-    #
-    #  The following is for project-wide static files in kadi/static/.
-    join(BASE_DIR, 'kadi/static'),
-)
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # insert your TEMPLATE_DIRS here
-            join(BASE_DIR, 'kadi/templates'),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'debug': True,
-            'context_processors': [
-                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                # list if you haven't customized them:
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
-            ],
-        },
-    },
-]
-# (1_8.W001) The standalone TEMPLATE_* settings were deprecated in Django 1.8
-# and the TEMPLATES dictionary takes precedence. You must put the values of the
-# following settings into your default TEMPLATES dict: TEMPLATE_DEBUG.
