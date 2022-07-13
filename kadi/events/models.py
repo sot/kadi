036daa5a5ed7666425b2ae63fbd825dfca13b1d7
Tom Aldcroft <taldcroft@gmail.com>
2022-07-13 19:44:25 -0400

    Restore good import order

--- kadi/events/models.py
+++ kadi/events/models.py
@@ -1,8 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import operator
 import sys
-from pathlib import Path
 from itertools import count
+from pathlib import Path
 
 import pyyaks.logger
 from django.db import models
