f33faee19aa96aca96f09e479991b950a33ab70c
Tom Aldcroft <taldcroft@gmail.com>
2025-01-08 07:05:49 -0500

    Improve compare_events

--- validate/events/compare_events_ecsv.py
+++ validate/events/compare_events_ecsv.py
@@ -90,16 +90,35 @@ def compare_ecsv_files(ecsv_file_a, ecsv_file_b, time_diff_allowed):
         return not_ok, "Column names do not match.", rows_a, rows_b
 
     for name in data_a.colnames:
-        if np.all(data_a[name] == data_b[name]):
+        vals_a = data_a[name]
+        vals_b = data_b[name]
+        if hasattr(vals_a, "mask"):
+            vals_a = vals_a.filled()
+        if hasattr(vals_b, "mask"):
+            vals_b = vals_b.filled()
+
+        vals_equal = (
+            np.isclose(vals_a, vals_b, rtol=0, atol=1e-3)
+            if vals_a.dtype.kind == "f"
+            else vals_a == vals_b
+        )
+        if np.all(vals_equal):
             continue
-        if name in ["start", "stop", "tstart", "tstop"]:
+
+        if name.endswith(("start", "stop")) or name == "manvr":
             time_diff = (CxoTime(data_a[name]) - CxoTime(data_b[name])).to_value(u.s)
-            time_diff_max = np.max(abs(time_diff))
+            idxs_bad = np.where(np.abs(time_diff) > time_diff_allowed)[0]
+            time_diff_max = np.max(np.abs(time_diff))
             # print(f"Max time difference for {ecsv_file_a.name}.{name}: {time_diff_max}")
             if time_diff_max > time_diff_allowed:
+                times_bad = CxoTime(data_a["start"][idxs_bad]).date
+                msg = (
+                    f"{name!s} time difference {time_diff_max:.1f} greater "
+                    f"than {time_diff_allowed} seconds at rows {idxs_bad} at {times_bad}."
+                )
                 return (
                     not_ok,
-                    f"Time difference {time_diff_max} greater than {time_diff_allowed} seconds.",
+                    msg,
                     rows_a,
                     rows_b,
                 )
@@ -107,16 +126,30 @@ def compare_ecsv_files(ecsv_file_a, ecsv_file_b, time_diff_allowed):
             diff = abs(data_a[name] - data_b[name])
             diff_max = np.max(diff)
             if diff_max > time_diff_allowed:
+                msg = f"{name!s} difference {diff_max:.1f} greater than {time_diff_allowed}."
                 return (
                     not_ok,
-                    f"Duration difference {diff_max} greater than {time_diff_allowed}.",
+                    msg,
                     rows_a,
                     rows_b,
                 )
         elif name == "notes":
             continue
         else:
-            return not_ok, f"Column {name} does not match.", rows_a, rows_b
+            idxs_bad = np.where(~vals_equal)[0]
+            vals_a_bad = vals_a[idxs_bad]
+            vals_b_bad = vals_b[idxs_bad]
+            times_bad = CxoTime(data_a["start"][idxs_bad]).date
+            msg = (
+                f"Column {name} does not match {vals_a_bad.data} vs {vals_b_bad.data} at "
+                f"rows {idxs_bad} at {times_bad}."
+            )
+            return (
+                not_ok,
+                msg,
+                rows_a,
+                rows_b,
+            )
 
     return "OK", "", rows_a, rows_b
 
