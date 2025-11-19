30582300218c95e780436c1347cc7dcbc49f7c5f
Tom Aldcroft <taldcroft@gmail.com>
2025-11-19 07:14:25 -0500

    Remove support for use_ska_dir (no longer needed)

--- kadi/commands/commands_v2.py
+++ kadi/commands/commands_v2.py
@@ -19,7 +19,6 @@ import numpy as np
 import requests
 from astropy.table import Table
 from cxotime import CxoTime
-from parse_cm.paths import load_dir_from_load_name
 from ska_helpers.retry import retry_func
 from ska_sun import get_nsm_attitude
 from testr.test_helper import has_internet
@@ -1304,7 +1303,6 @@ def get_load_cmds_from_occweb_or_local(
     dir_year_month=None,
     load_name=None,
     *,
-    use_ska_dir=False,
     archive=True,
 ) -> CommandTable:
     """Get the load cmds (backstop) for ``load_name`` within ``dir_year_month``
@@ -1339,36 +1337,23 @@ def get_load_cmds_from_occweb_or_local(
                 cmds = pickle.load(fh)
             return cmds
 
-    if use_ska_dir:
-        ska_dir = load_dir_from_load_name(load_name)
-        for filename in ska_dir.glob("CR????????.backstop"):
-            backstop_text = filename.read_text()
-            logger.info(f"Got backstop from {filename}")
+    load_dir_contents = occweb.get_occweb_dir(dir_year_month / load_name, timeout=5)
+    for filename in load_dir_contents["Name"]:
+        if re.match(r"CR\d{3}.\d{4}\.backstop", filename):
+            # Download the backstop file from OCCweb
+            backstop_text = occweb.get_occweb_page(
+                dir_year_month / load_name / filename,
+                cache=conf.cache_loads_in_astropy_cache,
+                timeout=10,
+            )
             cmds = parse_backstop(load_name, backstop_text)
             if archive:
                 write_backstop(cmds, cmds_filename)
             break
-        else:
-            raise ValueError(f"No backstop file found in {ska_dir}")
-
-    else:  # use OCCweb
-        load_dir_contents = occweb.get_occweb_dir(dir_year_month / load_name, timeout=5)
-        for filename in load_dir_contents["Name"]:
-            if re.match(r"CR\d{3}.\d{4}\.backstop", filename):
-                # Download the backstop file from OCCweb
-                backstop_text = occweb.get_occweb_page(
-                    dir_year_month / load_name / filename,
-                    cache=conf.cache_loads_in_astropy_cache,
-                    timeout=10,
-                )
-                cmds = parse_backstop(load_name, backstop_text)
-                if archive:
-                    write_backstop(cmds, cmds_filename)
-                break
-        else:
-            raise ValueError(
-                f"Could not find backstop file in {dir_year_month / load_name}"
-            )
+    else:
+        raise ValueError(
+            f"Could not find backstop file in {dir_year_month / load_name}"
+        )
 
     return cmds
 
